61c6e5dabe90d7a1186b6a03976fe3d8beafaeeb
Akhil Raj <akhilraj916@gmail.com>
2025-12-26 15:30:19 +0530

    added build directory

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
   base: mode === "github" ? "/FootBall_League_Manager/" : "/",
+  build: {
+    outDir: "dist", // 👈 this generates dist folder
+    emptyOutDir: true,
+  }
 }))
+
 
